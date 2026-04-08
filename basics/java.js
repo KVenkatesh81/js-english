5962897660361d8d96baa6953d041fce9c375b32
KVenkatesh81 <24je0806@iitism.ac.in>
2026-04-08 18:29:13 +0000

    done

--- basics/java.js
+++ basics/java.js
@@ -1,1 +1,13 @@
+"use strict ";
 console.log("venky");
+let x = null;
+let name = "Tammy";
+const found = false;
+
+// => Tammy, false, null
+console.log(name, found, x);
+
+var a;
+console.log(a); // => undefined
+var c=30;
+console.warn(c)
