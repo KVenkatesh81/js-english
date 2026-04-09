5cd96a48ba9874ba7e831ba2af674e3771b11fa3
KVenkatesh81 <24je0806@iitism.ac.in>
2026-04-09 14:47:22 +0000

    till math lib

--- basics/java.js
+++ basics/java.js
@@ -20,3 +20,20 @@ console.log(s)
 console.warn(ch)
 console.log(+true)
 console.log(+false)
+let mov = "tun tun"
+let com = mov
+com ="chutki"
+console.log(mov)
+let z = {
+    ki : "hi",
+    woman:"bye"
+}
+let ans = z
+z.ki ="ta ta "
+console.log(ans.ki)
+let so = Math.max(3,5,45,2)
+console.log(Math)
+let max =20
+let min =10
+/*to print random numbers between min and max the formula is console.log((Math.floor(Math.random)*(max-min+1))+min)    */
+console.log(Math.floor((Math.random()*(max-min)+1)+min))
